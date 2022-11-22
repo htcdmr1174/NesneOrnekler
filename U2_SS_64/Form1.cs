27b09e9af4bc24bd7f791ea394b32cb5e5a38e12
BIL-LAB5PC03\Ogrenci <123@123.COM>
2022-11-22 12:02:32 +0300

    ListBox nesnesinin içine sırasıyla 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110 sayılarını ekleyen program
    ListBox nesnesinin içine sırasıyla 110, 100, 90, 80, 70, 60, 50, 40, 30, 20, 10 sayılarını ekleyen
    program

--- U2_SS_64/Form1.cs
+++ U2_SS_64/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace U2_SS_63
+namespace U2_SS_64
 {
     public partial class Form1 : Form
     {
@@ -20,7 +20,7 @@ namespace U2_SS_63
         private void button1_Click(object sender, EventArgs e)
         {
 
-            for (int i = 10; i <= 100; i = i + 10)
+            for (int i = 110; i >= 10; i = i - 10)
             {
                 listBox1.Items.Add(i);
             }
