42d811bb76b7f8f357a454ff2b367e91eb8b4c09
BIL-LAB5PC03\Ogrenci <123@123.COM>
2022-11-29 11:36:17 +0300

    odev vücut kitle endesi

--- U2_Goldsoru/Form1.cs
+++ U2_Goldsoru/Form1.cs
@@ -16,5 +16,40 @@ namespace U2_Goldsoru
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            for (int i = 0; i < 20; i++)
+            {
+                listBox1.Items.Add(random.Next(1, 100));
+            }
+    
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int sayaç = 0;
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                if (Convert.ToInt32(listBox1.Items[i]) % 5 == 0)
+                {
+                    listBox4.Items.Add(listBox1.Items[i]);
+                }
+
+                if (Convert.ToInt32(listBox1.Items[i]) % 3 == 0)
+                {
+                    listBox3.Items.Add(listBox1.Items[i]);
+                }
+
+                if (Convert.ToInt32(listBox1.Items[i]) % 2 == 0)
+                {
+                    listBox2.Items.Add(listBox1.Items[i]);
+                }
+                else
+                    listBox5.Items.Add(listBox1.Items[i]);
+            }
+        }
     }
 }
---
+++ U2_Odev_2/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace U2_Odev_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double boy = Convert.ToDouble(txtboy.Text);
+            int kilo = Convert.ToInt32(txtkilo.Text);
+
+            double oran = kilo / boy;
+            
+            if (oran < 18.5)
+            {
+               MessageBox.Show( "zayıf");
+            }
+            else if (oran < 25)
+            {
+                MessageBox.Show("orta");
+            }
+            else if (oran < 30)
+            {
+                MessageBox.Show("kilolu");
+            }
+            else
+            {
+                
+
+            }
+        }
+    }
+}
