6ef2dc7238aa834d72918b72d2a1e577ce60f63d
BIL-LAB5PC03\Ogrenci <123@123.COM>
2022-12-20 16:16:17 +0300

    no message

--- U2_Goldsoru_8/Form1.cs
+++ U2_Goldsoru_8/Form1.cs
@@ -12,9 +12,67 @@ namespace U2_Goldsoru_8
 {
     public partial class Form1 : Form
     {
+        int sayi;
+        int tutulan;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            sayi = 100;
+            button2.Enabled = true;
+            for (int i = 0; i < 100; i++)
+            {
+                tutulan  = random.Next(100);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            { 
+                int tahmin = Convert.ToInt32(textBox1.Text);
+                if (tahmin < sayi)
+                {
+                    MessageBox.Show("yukarı");
+                    sayi = sayi - 10;
+             
+                }
+                else if (tahmin<tutulan)
+                {
+                    MessageBox.Show("aşagı");
+                    sayi = sayi - 10;
+                }
+                else
+                {
+                    MessageBox.Show("tebrikler ayrıcalık kazandınız ");
+
+                }
+                if (sayi==0)
+                {
+                    MessageBox.Show("oyunu bitirdiniz tekrardan başlayınız");
+                    button2.Enabled = false;
+
+                }
+                label1.Text = sayi.ToString();
+                
+             }
+
+            catch (Exception)
+            {
+                MessageBox.Show("lütfen sayı giriniz");
+
+                
+            }
+
+
+
+
+           
+
         }
     }
 }
