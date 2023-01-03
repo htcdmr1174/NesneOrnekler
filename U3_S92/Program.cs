927185f33c112e9ff1c2fd1cbf12fa52dcffc90e
BIL-LAB1PC08\Ogrenci <123@123.COM>
2023-01-03 16:38:58 +0300

    no message

---
+++ U3_S92/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U3_S92
+{
+    class Kisi
+    {
+        int yas;
+        string ad;
+        public Kisi()
+        {
+            yas = 19;
+            ad = "selim";
+            Console.WriteLine("Yapıcı metot çalıştı");
+        }
+        public int Yas
+
+        {
+            get
+            {
+                return yas;
+            }
+        }
+        public string Ad
+        {
+            get
+            {
+                return ad;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("program Başladı");
+            Kisi k = new Kisi();
+            Console.WriteLine("Adı:{0},Yaşı:{1}", k.Ad, k.Yas);
+            Console.WriteLine("program bitti.");
+            Console.ReadLine();
+        }
+    }  }
