a810738a68c45f1d30832c3c242078a7860d3b90
BIL-LAB5PC03\Ogrenci <123@123.COM>
2023-01-10 15:32:04 +0300

    no message

---
+++ U3_S105/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U3_S105
+{
+    public abstract class MotorluArac
+    {
+        public int MotorHacmi { get; set; }
+        public int ModelYili { get; set; }
+
+        public abstract void Calıs();
+        public abstract void Dur();
+
+    }
+    public class Otomobil : MotorluArac
+    {
+        public bool OtomatikVites { get; set; }
+        public override void Calıs()
+        {
+            Console.WriteLine("otomobil çalıştı ");
+
+        }
+        public override void Dur()
+        {
+            Console.WriteLine("otomobil durdu ");
+         }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Otomobil oto = new Otomobil
+            {
+                ModelYili = 2020,
+                MotorHacmi = 1600,
+                OtomatikVites = true
+            };
+            oto.Calıs();
+            oto.Dur();
+            Console.ReadLine();
+
+
+
+        }
+    }
+
+}
---
+++ U3_SS_106/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U3_SS_106
+{
+    public abstract class KütüpHane
+    {
+        public string Kitapadi { get; set; }
+        public string dergiadi { get; set; }
+        public string ansiklobedi { get; set; }
+        public abstract void oku();
+
+    }
+    public class Kitap : KütüpHane
+    {
+        public override void oku()
+        {
+            Console.WriteLine("kitap okundu ");
+
+        }
+    }
+    public class Dergi : KütüpHane
+    {
+        public override void oku()
+        {
+            Console.WriteLine("dergi okundu");
+        }
+    }
+        public class Ansklobedi: KütüpHane
+        {
+          public override void oku()
+          {
+            Console.WriteLine("ansiklobedi okundu");
+           }
+
+
+        }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kitap k = new Kitap();
+            k.Kitapadi = "paronaya";
+            Console.Write( "{0} adi",k.Kitapadi );
+            k.oku();
+
+
+            Dergi d = new Dergi();
+            d.dergiadi = "çukur";
+            Console.Write("{0} ad", d.dergiadi );
+            d.oku();
+
+            Ansklobedi a = new Ansklobedi();
+            a.ansiklobedi = "temizlikçi";
+            Console.Write("{0} adı", a.ansiklobedi);
+            a.oku();
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
