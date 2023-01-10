c30aa02ed8f93232cf7a831d177317f2add30cef
BIL-LAB5PC03\Ogrenci <123@123.COM>
2023-01-10 16:05:39 +0300

    no message

---
+++ U2_Uyg32/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace U2_Uyg32
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string kullaniciAdi;
+            long parola;
+
+            try
+            {
+                kullaniciAdi = textBox1.Text.ToString();
+                parola = long.Parse(textBox2.Text.ToString());
+                MessageBox.Show("Giriş Başarılı. Hoşgeldiniz " + kullaniciAdi);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("sifreniz sadece sayılardan oluşmalıdır Tekrar deneyiniz.");
+                textBox2.Text = "";
+            }
+            finally
+            { }
+        }
+    }
+}
+
