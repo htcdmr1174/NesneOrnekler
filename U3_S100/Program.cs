f3c9b30502e2c68798f67d72e054450dd9273974
BIL-LAB5PC03\Ogrenci <123@123.COM>
2023-01-10 11:47:41 +0300

    no message

--- U3_S100/Program.cs
+++ U3_S100/Program.cs
@@ -6,10 +6,37 @@ using System.Threading.Tasks;
 
 namespace U3_S100
 {
-    class Program
+    class OkulPersonel
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+
+    }
+    class Ogretmen : OkulPersonel
+    {
+        public string Brans { get; set; }
+
+    }
+
+    class program
     {
         static void Main(string[] args)
         {
+            OkulPersonel per = new OkulPersonel();
+            Ogretmen ogrt = new Ogretmen();
+
+            ogrt.Ad = "serkan";
+            ogrt.Soyad = "aydın";
+            ogrt.Brans = "Bilişim";
+            Console.WriteLine("ögretmen sınıfından turetilen  {0} {1} {2}", ogrt.Ad, ogrt.Soyad, ogrt.Brans);
+
+
+            per.Ad = "ramazan";
+            per.Soyad ="güneş";
+            Console.WriteLine("OkulPersonoli sınavda türetilen {0} {1}", per.Ad, per.Soyad);
+            Console.ReadLine();
+
         }
+
     }
 }
