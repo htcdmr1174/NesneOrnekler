a49868600bec4b8d7ead3c19318a5632318c1486
BIL-LAB1PC10\Ogrenci <123@123.com>
2023-03-07 11:06:43 +0300

    no message

--- U4_Uyg7/Form1.cs
+++ U4_Uyg7/Form1.cs
@@ -16,5 +16,22 @@ namespace U4_Uyg7
         {
             InitializeComponent();
         }
+        string[] isimler = new string[3];
+        int[,] notlar = new int[3, 4];
+        int index = 0;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double toplam;
+            for (int x = 0; x < 3; x++)
+            {
+                toplam = 0;
+                for (int y = 0; y < 4; y++)
+                {
+                    toplam += notlar[x, y];
+                }
+                listBox1.Items.Add(isimler[x] + "=>" + toplam / 4);
+
+            }
+        }
     }
 }
---
+++ U4_Uyg8/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace U4_Uyg8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        byte[,] dizi = new byte[4, 4];
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random rasgele = new Random();
+            int satırRasgele = rasgele.Next(4);
+            int sutunRasgele = rasgele.Next(4);
+            dizi[satırRasgele, sutunRasgele] = 1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte satır = byte.Parse(textBox1.Text);
+            byte sutun = byte.Parse(textBox2.Text);
+            PictureBox kutu = this.Controls.Find("p" + satır + sutun, true)[0] as PictureBox;
+            byte durum = dizi[satır, sutun];
+            if (durum == 0)
+            { kutu.BackColor = Color.Green; }
+            else
+            { kutu.BackColor = Color.Red; }
+        }
+    }
+}
