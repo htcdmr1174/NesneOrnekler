dbe864e4ecafb657db77ea1ced5318bbecbd9d10
BIL-LAB1PC09\Ogrenci <123@123.COM>
2023-03-14 16:17:47 +0300

    no message

---
+++ U5_Uyg6/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace U5_Uyg6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.anaMenu = new System.Windows.Forms.MenuStrip();
+            this.menu = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.anaMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // anaMenu
+            // 
+            this.anaMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menu,
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3});
+            this.anaMenu.Location = new System.Drawing.Point(0, 0);
+            this.anaMenu.Name = "anaMenu";
+            this.anaMenu.Size = new System.Drawing.Size(800, 24);
+            this.anaMenu.TabIndex = 0;
+            // 
+            // menu
+            // 
+            this.menu.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(50, 20);
+            this.menu.Text = "dosya";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(51, 20);
+            this.toolStripMenuItem2.Text = "düzen";
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(42, 20);
+            this.toolStripMenuItem3.Text = "çıkış";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.anaMenu);
+            this.MainMenuStrip = this.anaMenu;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.anaMenu.ResumeLayout(false);
+            this.anaMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip anaMenu;
+        private System.Windows.Forms.ToolStripMenuItem menu;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+    }
+}
+
