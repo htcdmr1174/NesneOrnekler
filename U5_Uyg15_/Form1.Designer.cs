ab6d475b962e709d4b34f27d119d7ce64db64fcb
BIL-LAB1PC09\Ogrenci <123@123.COM>
2023-04-11 14:30:16 +0300

    no message

--- U5_Uyg15_/Form1.Designer.cs
+++ U5_Uyg15_/Form1.Designer.cs
@@ -73,36 +73,36 @@ namespace U5_Uyg15_
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(284, 114);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(38, 13);
             this.label1.TabIndex = 4;
-            this.label1.Text = "label1";
+            this.label1.Text = "Adınız;";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(284, 152);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(55, 13);
             this.label2.TabIndex = 5;
-            this.label2.Text = "label2";
+            this.label2.Text = "Soyadınız;";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(287, 232);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(37, 13);
             this.label3.TabIndex = 7;
-            this.label3.Text = "label3";
+            this.label3.Text = "Aders;";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(284, 193);
+            this.label4.Location = new System.Drawing.Point(253, 196);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.Size = new System.Drawing.Size(86, 13);
             this.label4.TabIndex = 6;
-            this.label4.Text = "label4";
+            this.label4.Text = "Dogum Tarihiniz;";
             // 
             // button1
             // 
@@ -110,7 +110,7 @@ namespace U5_Uyg15_
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 42);
             this.button1.TabIndex = 8;
-            this.button1.Text = "button1";
+            this.button1.Text = "KAYDET";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
