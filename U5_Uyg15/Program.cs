50d8280bf77bd2580f5c2f74fe69f0ebebd678ea
BIL-LAB1PC09\Ogrenci <123@123.COM>
2023-04-11 11:23:07 +0300

    no message

---
+++ U5_Uyg15/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U5_Uyg15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ U5_Uyg15_/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace U5_Uyg15_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ToolTip tt = new ToolTip();
+            tt.SetToolTip(textBox1, "adınızı giriniz");
+            tt.SetToolTip(textBox2,"soy adınızı giriniz");
+            tt.SetToolTip(textBox3, "dogum tarihinizi  gg/aa/yyyy şeklinde giriniz");
+            tt.SetToolTip(textBox4, "adresinizi giriniz");
+            tt.SetToolTip(button1, "kaydetmek için tıklayınız");
+
+
+        }
+    }
+}
