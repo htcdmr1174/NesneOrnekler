7dc438b58de9c7c50c38c88f2e94f57d9fd1a076
BIL-LAB1PC09\Ogrenci <123@123.COM>
2023-04-25 11:26:24 +0300

    no message

---
+++ U5_Uyg23/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace U5_Uyg23
+{
+  
+
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+         List<Sehirler> listSehirler = new List<Sehirler>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Sehirler sehir = new Sehirler();
+            sehir.Plaka = textBox1.Text;
+            sehir.SehirAdi = textBox2.Text;
+            listSehirler.Add(sehir);
+            Bagla();
+        }
+            private void Bagla()
+            {
+                comboBox1.DataSource = null;
+                comboBox1.DataSource = listSehirler;
+                comboBox1.DisplayMember = "SehirAdi";
+                comboBox1.ValueMember = "Plaka";
+           
+        }
+    }
+}
