863667377815cec69e1383a9d7c14aa90eac4e1e
BIL-LAB1PC09\Ogrenci <123@123.COM>
2023-05-16 16:32:04 +0300

    no message

--- KutuphaneProjesi/formKitapTur.Designer.cs
+++ KutuphaneProjesi/formKitapTur.Designer.cs
@@ -29,12 +29,133 @@ namespace KutuphaneProjesi
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtTurAdi = new System.Windows.Forms.TextBox();
+            this.gridKitapTur = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridKitapTur)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Controls.Add(this.button3);
+            this.groupBox3.Location = new System.Drawing.Point(59, 123);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(651, 118);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "İşlemler";
+            // 
+            // button1
+            // 
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.ImageIndex = 2;
+            this.button1.Location = new System.Drawing.Point(242, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 69);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "SİL";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.ImageIndex = 0;
+            this.button2.Location = new System.Drawing.Point(37, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(181, 69);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "KAYDET";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.ImageIndex = 1;
+            this.button3.Location = new System.Drawing.Point(420, 43);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(151, 69);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "GÜNCELLE";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.txtTurAdi);
+            this.groupBox2.Location = new System.Drawing.Point(140, 23);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(438, 94);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Arama";
+            // 
+            // txtTurAdi
+            // 
+            this.txtTurAdi.Location = new System.Drawing.Point(138, 37);
+            this.txtTurAdi.Name = "txtTurAdi";
+            this.txtTurAdi.Size = new System.Drawing.Size(132, 20);
+            this.txtTurAdi.TabIndex = 15;
+            // 
+            // gridKitapTur
+            // 
+            this.gridKitapTur.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridKitapTur.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.gridKitapTur.Location = new System.Drawing.Point(59, 247);
+            this.gridKitapTur.Name = "gridKitapTur";
+            this.gridKitapTur.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.gridKitapTur.Size = new System.Drawing.Size(651, 191);
+            this.gridKitapTur.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Tür Adı:";
+            // 
+            // formKitapTur
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gridKitapTur);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox3);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "formKitapTur";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "formKitapTur";
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridKitapTur)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtTurAdi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView gridKitapTur;
     }
 }
